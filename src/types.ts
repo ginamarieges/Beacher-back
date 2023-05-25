1424e05265547a961b8100dc92593112c48a4079
Gina Marieges <gina.marieges@gmail.com>
2023-05-25 20:43:37 +0200

    Add loginUser controller

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,16 @@
+import { type Request } from "express";
+
 export interface PathsStructure {
   pingController: string;
 }
+
+export interface UserCredentials {
+  username: string;
+  password: string;
+}
+
+export type UserCredentialsRequest = Request<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  UserCredentials
+>;
