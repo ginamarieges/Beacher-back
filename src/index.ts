f70e4611a548016b179014afffa73f4cada4e996
Gina Marieges <gina.marieges@gmail.com>
2023-05-25 10:29:31 +0200

    Add port 4000 and debug

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "./loadEnvironment.js";
+import createDebug from "debug";
+import { app } from "./server/index.js";
+
+const debug = createDebug("beaches-api:root");
+
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  debug(`Listening to port ${port}`);
+});
