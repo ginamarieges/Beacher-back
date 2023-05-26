b9bbb2a34c5811112a662a120d47ea12f7186494
Gina Marieges <gina.marieges@gmail.com>
2023-05-26 22:21:56 +0200

    Add error of token not found in responseData

--- src/utils/responseData/responseData.ts
+++ src/utils/responseData/responseData.ts
@@ -5,4 +5,5 @@ export const responseErrorData: ErrorStructure = {
   wrongCredentials: new CustomError("Wrong credentials", 401),
   endpointNotFound: new CustomError("Endpoint not found", 404),
   serverError: new Error("Server error"),
+  tokenNotFound: new CustomError("Token not found", 401),
 };
--- src/utils/responseData/types.ts
+++ src/utils/responseData/types.ts
@@ -4,4 +4,5 @@ export interface ErrorStructure {
   endpointNotFound: CustomError;
   wrongCredentials: CustomError;
   serverError: Error;
+  tokenNotFound: CustomError;
 }
