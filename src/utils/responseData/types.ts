0469652912b98d825e5740b9c33fd4359126781f
Gina Marieges <gina.marieges@gmail.com>
2023-05-26 22:00:40 +0200

    Add new error in responseData

--- src/utils/responseData/types.ts
+++ src/utils/responseData/types.ts
@@ -3,4 +3,5 @@ import type CustomError from "../../CustomError/CustomError.js";
 export interface ErrorStructure {
   endpointNotFound: CustomError;
   wrongCredentials: CustomError;
+  serverError: Error;
 }
