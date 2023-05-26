ff21925701def63729cf51b97533b3148409dacd
Gina Marieges <gina.marieges@gmail.com>
2023-05-26 10:01:36 +0200

    Create responseData and its type

---
+++ src/utils/responseData.ts
@@ -0,0 +1,7 @@
+import CustomError from "../CustomError/CustomError.js";
+import { type ErrorStructure } from "./types.js";
+
+export const responseErrorData: ErrorStructure = {
+  wrongCredentials: new CustomError("Wrong credentials", 401),
+  endpointNotFound: new CustomError("Endpoint not found", 404),
+};
