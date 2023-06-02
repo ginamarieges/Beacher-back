bc79d0d60bb72280831caa1df7777fd08e1bdb5d
Gina Marieges <gina.marieges@gmail.com>
2023-06-02 16:12:42 +0200

    Modify beaches path in app and router

--- src/server/routers/beaches/beachesRouter.ts
+++ src/server/routers/beaches/beachesRouter.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import paths from "../../paths/paths.js";
-import auth from "../../middlewares/authMiddleware/authMiddleware.js";
 import { getBeaches } from "../../controllers/beachesControllers/beachesControllers.js";
 
 const beachesRouter = Router();
 
-beachesRouter.get(paths.getBeaches, auth, getBeaches);
+beachesRouter.get("/", getBeaches);
 
 export default beachesRouter;
