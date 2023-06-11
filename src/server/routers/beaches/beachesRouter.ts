1c3f656a4058738228881d15fe4e86a61c3d2dbb
Gina Marieges <gina.marieges@gmail.com>
2023-06-11 20:31:50 +0200

    Add filter in getBeaches and remove filter controller

--- src/server/routers/beaches/beachesRouter.ts
+++ src/server/routers/beaches/beachesRouter.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import {
   addBeach,
   deleteBeach,
-  filterBeaches,
   getBeaches,
 } from "../../controllers/beachesControllers/beachesControllers.js";
 import paths from "../../paths/paths.js";
@@ -12,8 +11,6 @@ import addBeachSchema from "../../../utils/Schemas/addBeachSchema.js";
 const beachesRouter = Router();
 
 beachesRouter.get("/", getBeaches);
-
-beachesRouter.get(paths.filter, filterBeaches);
 
 beachesRouter.delete(paths.delete, deleteBeach);
 
